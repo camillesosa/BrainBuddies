ba730ce1d37400c5ec6c6974e1e3f273d42f900d
camillesosa <71521586+camillesosa@users.noreply.github.com>
2023-12-07 23:17:07 -0800

    Changing how video will be selected

--- static/selectVideo.php
+++ static/selectVideo.php
@@ -3,56 +3,81 @@
 require_once "config.php";
 
 // Define variables and initialize with empty values
-$videoOne = $videoTwo = $videoThree = $videoFour = $videoFive = 0;
+$animeGamesVideo = $artsVideo = $foodMusicVideo = $outdoorsVideo = $sportsVideo = 0;
 $selectedVideo = "";
 
-$anime = trim($_POST["anime"]);
-$cosplay = trim($_POST["cosplay"]);
-$larping = trim($_POST["larping"]);
-$videogames = trim($_POST["videogames"]);
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (isset($_POST['hobbies'])) {
+        // Get the selected hobbies
+        $selectedHobbies = $_POST['hobbies'];
 
-$arts = trim($_POST["arts-crafts"]);
-$collecting = trim($_POST["collecting"]);
-$fashion = trim($_POST["fashion"]);
-$photo = trim($_POST["photography"]);
+        foreach ($selectedHobbies as $hobby) {
+            // Increment the corresponding video tag for certain hobbies
+            if ($hobby == 'anime' || 'cosplay' || 'larping' || 'videogames') {
+                $animeGamesVideo = $animeGamesVideo + 1;
+            }
+            if ($hobby == 'arts-crafts' || 'collecting' || 'fashion' || 'photography') {
+                $artsVideo = $artsVideo + 1;
+            }
+            if ($hobby == 'dancing' || 'exercise' || 'outdoors') {
+                $outdoorsVideo = $outdoorsVideo + 1;
+            }
+            if ($hobby == 'sports' || 'extreme-sports') {
+                $sportsVideo = $sportsVideo + 1;
+            }
+            if ($hobby == 'food' || 'gardening' || 'literature' || 'movies' || 'music') {
+                $foodMusicVideo = $foodMusicVideo + 1;
+            }
+        }
+    } else {
+        echo "No hobbies selected.";
+    }
 
-$dancing = trim($_POST["dancing"]);
-$exercise = trim($_POST["exercise"]);
-$outdoors = trim($_POST["outdoors"]);
+    if (isset($_POST['bud-hobbies'])) {
+        // Get the selected hobbies
+        $selectedBudHobbies = $_POST['bud-hobbies'];
 
-$sports = trim($_POST["sports"]);
-$xtreme = trim($_POST["extreme-sports"]);
+        foreach ($selectedBudHobbies as $bhobby) {
+            // Increment the corresponding video tag for certain hobbies
+            if ($bhobby == 'banime' || 'bcosplay' || 'blarping' || 'bvideogames') {
+                $animeGamesVideo = $animeGamesVideo + 1;
+            }
+            if ($bhobby == 'barts-crafts' || 'bcollecting' || 'bfashion' || 'bphotography') {
+                $artsVideo = $artsVideo + 1;
+            }
+            if ($bhobby == 'bdancing' || 'bexercise' || 'boutdoors') {
+                $outdoorsVideo = $outdoorsVideo + 1;
+            }
+            if ($bhobby == 'bsports' || 'bextreme-sports') {
+                $sportsVideo = $sportsVideo + 1;
+            }
+            if ($bhobby == 'bfood' || 'bgardening' || 'bliterature' || 'bmovies' || 'bmusic') {
+                $foodMusicVideo = $foodMusicVideo + 1;
+            }
+        }
+    } else {
+        echo "No hobbies selected.";
+    }
 
-$food = trim($_POST["food"]);
-$gardening = trim($_POST["gardening"]);
-$lit = trim($_POST["literature"]);
-$movies = trim($_POST["movies"]);
-$music = trim($_POST["music"]);
+    // Compare the int video values and pick the highest one
+    $maxValue = max($animeGamesVideo, $artsVideo, $foodMusicVideo, $outdoorsVideo, $sportsVideo);
+
+    // If there is a tie, it'll pick the video it encounters first
+    if ($maxValue == $animeGamesVideo) {
+        $selectedVideo = 'animeGamesVideo';
+    } elseif ($maxValue == $artsVideo) {
+        $selectedVideo = 'artsVideo';
+    } elseif ($maxValue == $foodMusicVideo) {
+        $selectedVideo = 'foodMusicVideo';
+    } elseif ($maxValue == $outdoorsVideo) {
+        $selectedVideo = 'outdoorsVideo';
+    } elseif ($maxValue == $sportsVideo) {
+        $selectedVideo = 'sportsVideo';
+    }
+
+}
 
 
-
-$banime = trim($_POST["banime"]);
-$bcosplay = trim($_POST["bcosplay"]);
-$blarping = trim($_POST["blarping"]);
-$bvideogames = trim($_POST["bvideogames"]);
-
-$barts = trim($_POST["barts-crafts"]);
-$bcollecting = trim($_POST["bcollecting"]);
-$bfashion = trim($_POST["bfashion"]);
-$bphoto = trim($_POST["bphotography"]);
-
-$bdancing = trim($_POST["bdancing"]);
-$bexercise = trim($_POST["bexercise"]);
-$boutdoors = trim($_POST["boutdoors"]);
-
-$bsports = trim($_POST["bsports"]);
-$bxtreme = trim($_POST["bextreme-sports"]);
-
-$bfood = trim($_POST["bfood"]);
-$bgardening = trim($_POST["bgardening"]);
-$blit = trim($_POST["bliterature"]);
-$bmovies = trim($_POST["bmovies"]);
-$bmusic = trim($_POST["bmusic"]);
 
 
 
